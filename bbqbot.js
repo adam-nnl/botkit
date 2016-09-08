a579031c5d02b7b660e7e61b628097793dcc92b4
Adam Lara <adam.nnl@gmail.com>
2016-09-07 22:47:05 -0500

    Update bbqbot.js

--- bbqbot.js
+++ bbqbot.js
@@ -210,6 +210,7 @@ controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function
 
 controller.hears(['google (.*)'],
     'direct_message,direct_mention,mention', function(bot, message) {
+    var query = message.match[1]; 
     var search_str = require('querystring').escape(query);
         bot.reply(message,
             ':mag: Let me Google that for you!- ' + 'https://www.google.com/?#q=' + search_str );
