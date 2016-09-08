b33d65fbfc6f4af0375bfe3c30ff34fdb86696c4
Adam Lara <adam.nnl@gmail.com>
2016-09-07 22:52:10 -0500

    Update bbqbot.js

--- bbqbot.js
+++ bbqbot.js
@@ -208,7 +208,7 @@ controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function
     });
 });
 
-controller.hears('google (.*)',['message_received'],function(bot,message) {
+controller.hears('google (.*)','direct_message,direct_mention,mention',function(bot,message) {
   var query = message.match[1]; 
     var search_str = require('querystring').escape(query);
         bot.reply(message,
