7979af3c5ccf6d3f9772c59225a01cfa459a6e3c
Adam Lara <adam.nnl@gmail.com>
2016-09-07 22:21:53 -0500

    Update bbqbot.js

--- bbqbot.js
+++ bbqbot.js
@@ -64,6 +64,8 @@ if (!process.env.token) {
 var Botkit = require('./lib/Botkit.js');
 var os = require('os');
 
+var w2v = require( 'word2vec' );
+
 var controller = Botkit.slackbot({
     debug: true
 });
@@ -206,6 +208,13 @@ controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function
     });
 });
 
+controller.hears(['google', 'search', 'google', 'lmgtfy'],
+    'direct_message,direct_mention,mention', function(bot, message) {
+    var search_str = require('querystring').escape(message.text);
+        bot.reply(message,
+            ':mag: Let me Google that for you!- ' + 'https://www.google.com/?#q=' + search_str );
+
+    });
 
 controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
     'direct_message,direct_mention,mention', function(bot, message) {
