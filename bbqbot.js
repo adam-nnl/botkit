b8dfac031a69f43ad2b8fde80540507d36bc40fe
Adam Lara <adam.nnl@gmail.com>
2016-09-08 20:47:17 -0500

    Update bbqbot.js

--- bbqbot.js
+++ bbqbot.js
@@ -76,11 +76,21 @@ var bot = controller.spawn({
 controller.hears(['word2vec'],'direct_message', function(bot, message) {
 
     var word2vec = require('word2vec.js');
-    var analyzer = word2vec.analyzer('./GoogleNews-vectors-negative300.bin');
+    var analyzer = word2vec.analyzer('./vector.txt');
  
     // Find Word's Vector 
     var king = analyzer.findVec('berlin');
-    bot.reply(message,':robot_face: I am a bot named <@' + king);
+    var man = analyzer.findVec('germany');
+    var woman = analyzer.findVec('korea');
+ 
+    // Vector Operation 
+    var op = analyzer.operator.sub(king, man);
+    op = analyzer.operator.sum(op, woman);
+ 
+    // Find Cousins 
+    var cousins = analyzer.findCousin(op, 10);
+    //console.log(cousins);
+    bot.reply(message,':robot_face: I am a bot named <@' + cousins);
 
     });
 
